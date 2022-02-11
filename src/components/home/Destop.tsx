eb067a1273175115e057c983a909cf7d950f47e7
sunith <sunithvs2002@gmail.com>
2022-02-11 13:25:36 +0530

    almost finished

--- src/components/home/Destop.tsx
+++ src/components/home/Destop.tsx
@@ -9,7 +9,7 @@ export class Desktop extends React.Component
     {
         return (
             <>
-                <div className="p-0 d-flex">
+                <div className="p-0 d-flex overflow-hidden">
                     <div className='left-box '>
 
                         <Balloon/>
@@ -27,6 +27,7 @@ export class Desktop extends React.Component
                         <img src={banner} alt={"banner"} className={"banner-image"} />
                     </div>
                 </div>
+
 
             </>
         );
