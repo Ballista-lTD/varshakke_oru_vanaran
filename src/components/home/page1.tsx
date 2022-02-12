7fcb9a12003793824c025f87eaf5f17939c0564b
nihal <nihalm378@gmail.com>
2022-02-12 21:57:07 +0530

    bells devors

--- src/components/home/page1.tsx
+++ src/components/home/page1.tsx
@@ -39,18 +39,24 @@ class Page1Loc extends AuthComponent<Page1Props, AuthState>
                                 <img src={Build} className="align-self-start position-absolute w-75" alt="fire"/>
                             </div>
 
-                            <Typography fontFamily="Poppins" className="align-self-end mt-5 pt-3 text-wrap pe-4"
+                            <Typography fontFamily="Poppins" className="mt-5 pt-3 text-wrap pe-4"
                                 fontWeight="bolder"
                                 fontSize="28px" width="230px">HACKS
                                 TO GET MINGLED</Typography>
                         </div>}
                 </div>
-                <div className="py-0 pt-3 d-flex justify-content-center flex-column align-items-center">
+                <div className="d-flex justify-content-center flex-column align-items-center">
                     {!this.props.showBanner &&
-                        <Typography fontFamily="Poppins" className="align-self-end mt-5 pt-3 text-wrap pe-4"
-                            fontWeight="bolder"
-                            fontSize="28px" width="230px">HACKS
-                            TO GET MINGLED</Typography>}
+                        <div>
+                            <div>
+                                <img src={Build} className="fixed-top position-absolute w-50" alt="fire"/>
+                            </div>
+
+                            <Typography fontFamily="Poppins" className="text-wrap pe-4"
+                                fontWeight="bolder"
+                                fontSize="28px" width="230px">HACKS
+                                TO GET MINGLED</Typography>
+                        </div>}
                     <Typography fontFamily="Poppins" className="align-self-end mt-5 pt-3 w-100 text-wrap"
                         fontSize="21px">
                         <List className={"d-flex flex-column justify-content-center align-items-center"}>
