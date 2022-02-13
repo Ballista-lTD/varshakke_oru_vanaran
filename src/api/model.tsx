70e5e4e29805b825d114fc9dab2215fc048f50df
Rohit T P <tprohit9@gmail.com>
2022-02-14 00:13:31 +0530

    registration closed

--- src/api/model.tsx
+++ src/api/model.tsx
@@ -36,6 +36,42 @@ export class TokenObject extends ModelObject
     }
 }
 
+export class PartnerTokenObject extends ModelObject
+{
+    name = "";
+    intelligence = "";
+    strength = "";
+    beauty = "";
+    charisma = "";
+    wealth = "";
+    will_help_poor = "";
+    religiousity = "";
+    liberal = "";
+
+    modify = async (path: string, data = this.data) =>
+    {
+        try
+        {
+            this.setData();
+            const headers = {"Authorization": `Bearer ${getAuth()}`};
+            return await patch(`${this.baseUrl}${this.id}/${path}`, data, headers);
+        }
+        catch (e)
+        {
+            throw await (e as { json: () => Promise<unknown> }).json();
+        }
+
+    };
+
+    constructor(data: ModelData, baseUrl: string)
+    {
+        super(data, baseUrl);
+        this.fields = ["id", "intelligence", "strength", "beauty", "charisma", "wealth", "will_help_poor", "religiousity", "liberal"];
+        this.getData();
+    }
+}
+
+export const PartnerToken = new Model(baseUrl + "/api/token/others", TokenObject);
 export const Token = new Model(baseUrl + "/auth/token/", TokenObject);
 
 
--- src/components/home/page1.tsx
+++ src/components/home/page1.tsx
@@ -9,6 +9,7 @@ import Luv2 from "../../images/luv2.svg";
 import Slider from "../utils/slider/slider";
 import {AuthComponent, AuthPropsLoc, AuthState} from "../../api/auth";
 import {withRouter} from "react-router";
+import {toast, ToastContainer} from "react-toastify";
 
 interface ruless {
     rule: string;
@@ -38,6 +39,17 @@ class Page1Loc extends AuthComponent<Page1Props, AuthState>
 
         return (
             <div className=" overflow-hidden vh-100 " color="black">
+                <ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                />
                 <div className={`d-flex flex-row justify-content-between ${!this.props.showBanner ? "pt-5 mt-5 justify-content-center" : ""}`}>
                     {this.props.showBanner &&
                         <>
@@ -80,7 +92,7 @@ class Page1Loc extends AuthComponent<Page1Props, AuthState>
                         <img className="vw-100 align-bottom position-absolute py-0 " src={Fire} alt="fire"/>
                         <div className={"w-75 pt-5 mt-3"}>
                             <Slider text="Oo Sammathiche" text_unlocked="okey " color="#fff"
-                                onSuccess={() => setTimeout(() => this.props.history.push("/mingle"), 500)}
+                                onSuccess={() => toast.error("Registration closed")}
                             />
                         </div>
                     </div>
